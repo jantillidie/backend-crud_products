a12facb4869b188c7735b0ed96d1f03b44ac8fe7
Jan Maaß <git@antipost.de>
2023-12-03 18:41:06 +0100

    Add populate reviews

--- db/models/Product.js
+++ db/models/Product.js
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
+import "./Review.js";
 
 const { Schema } = mongoose;
 
-const ProductSchema = new Schema({
+const productSchema = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, default: 0 },
   currency: { type: String, default: "EUR" },
+  reviews: { type: [Schema.Types.ObjectId], ref: "Review" },
 });
 
-const Product = mongoose.models.Product || mongoose.model("Product", ProductSchema);
+const Product = mongoose.models.Product || mongoose.model("Product", productSchema);
 
 export default Product;
